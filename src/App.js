fc9e62cec3de8731c1a92633a25f78504fabdf54
raicleysantana <raicleysantana1@gmail.com>
2022-09-05 00:30:02 -0400

    Feature

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ const stages = [
     { id: 3, name: "end" },
 ];
 
+const guessesQty = 3;
+
 function App() {
     const [gameStage, setGameStage] = useState(stages[0].name);
     const [words] = useState(wordsList);
@@ -20,19 +22,20 @@ function App() {
     const [letters, setLetters] = useState([]);
     const [guessedLetters, setGuessedLetters] = useState([]);
     const [wrongLetters, setWrongLetters] = useState([]);
-    const [guesses, setGuesses] = useState(3);
+    const [guesses, setGuesses] = useState(guessesQty);
     const [score, setScore] = useState(0);
 
-    const pickWordAndCategory = () => {
+    const pickWordAndCategory = useCallback(() => {
         const categories = Object.keys(words);
         const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
-
         const word = words[category][Math.floor(Math.random() * words[category].length)];
 
         return { word, category };
-    }
+    }, [words]);
 
-    const startGame = () => {
+    const startGame = useCallback(() => {
+        clearLetterStates();
+
         const { word, category } = pickWordAndCategory();
 
         let wordLetters = word.split("");
@@ -43,7 +46,7 @@ function App() {
         setLetters(wordLetters);
 
         setGameStage(stages[1].name);
-    }
+    }, [pickWordAndCategory]);
 
     const verifyLetter = (letter) => {
         const normalizedLetter = letter.toLowerCase();
@@ -62,14 +65,47 @@ function App() {
                 ...actualWrongLetters,
                 normalizedLetter
             ]);
+
+            setGuesses((actualGuesses) => actualGuesses - 1);
         }
-        //setGameStage(stages[2].name);
+
+
+
     }
 
     const retry = () => {
+        setScore(0);
+        setGuesses(guessesQty);
         setGameStage(stages[0].name);
     }
 
+    const clearLetterStates = () => {
+        setGuessedLetters([]);
+        setWrongLetters([]);
+    }
+
+
+    useEffect(() => {
+
+        if (guesses === 0) {
+            clearLetterStates();
+            setGameStage(stages[2].name);
+            //Reset all game
+        }
+
+    }, [guesses]);
+
+    useEffect(() => {
+
+        const uniqueLetters = [...new Set(letters)];
+
+        if (guessedLetters.length === uniqueLetters.length) {
+            setScore((actualScore) => (actualScore += 100));
+
+            startGame();
+        }
+
+    }, [guessedLetters, letters, startGame]);
     return (
         <div className="App">
             {gameStage === "start" && <StartScreen startGame={startGame} />}
@@ -83,7 +119,7 @@ function App() {
                 guesses={guesses}
                 score={score}
             />}
-            {gameStage === "end" && <GameOver retry={retry} />}
+            {gameStage === "end" && <GameOver retry={retry} score={score} />}
         </div>
     );
 }
--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './GameOver.css';
 // import { Container } from './styles';
 
-const components = ({ retry }) => {
+const components = ({ retry, score }) => {
     return (
         <div>
-            <h1>Game fim</h1>
+            <h1>Fim de jogo</h1>
 
+            <h2>
+                A sua pontuação foi: <span>{score}</span>
+            </h2>
             <button onClick={retry}>Resetar jogo</button>
         </div>
     );
